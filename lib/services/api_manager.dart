2cd71959af635d8d1ab8d95571ab83793148cbf2
AlbertoYabeda <albertoacmacuacua@gmail.com>
2021-07-14 19:39:50 +0100

    created constants and models

--- lib/services/api_manager.dart
+++ lib/services/api_manager.dart
@@ -1,2 +1,15 @@
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'package:news_app/constants/strings.dart';
 
+class Api_Manager{
+
+  void getNews() async {
+    var client = http.Client();
+
+    var response =  await client.get(Uri.parse(Strings.api_news_url));
+
+    if(response.statusCode == 200){
+
+    }
+  }
+}
