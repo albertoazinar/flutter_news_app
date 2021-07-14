750ba8c349c8d4d9c05692e58715601a70bdf4b4
AlbertoYabeda <albertoacmacuacua@gmail.com>
2021-07-14 20:01:43 +0100

    finished api manager http  get method

--- lib/services/api_manager.dart
+++ lib/services/api_manager.dart
@@ -1,15 +1,24 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:news_app/constants/strings.dart';
+import 'package:news_app/models/news_info.dart';
 
 class Api_Manager{
 
-  void getNews() async {
+  Future<Article> getNews() async {
     var client = http.Client();
-
-    var response =  await client.get(Uri.parse(Strings.api_news_url));
-
-    if(response.statusCode == 200){
-
+    var articleModel = null;
+    try {
+      var response = await client.get(Uri.parse(Strings.api_news_url));
+      if (response.statusCode == 200) {
+        var jsonString = response.body;
+        var jsonMap = json.decode(jsonString);
+        articleModel = Article.fromJson(jsonMap);
+      }
+    }catch(err){
+      return articleModel;
     }
+    return articleModel;
   }
 }
